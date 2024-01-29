7873e29b5c5e688a9f2a94f9c079e447dbcb495e
Prakrit-Jain <prakrit440@gmail.com>
2024-01-30 00:02:00 +0530

    feat: organise code into different modules directories

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -3,32 +3,7 @@
     perspective of the parent module
 */
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() { // need to add pub to give visiblity to the module
-
-        }
-    }
-
-    //---------------------------------
-
-    pub struct Breakfast {
-        pub toast: String, // need to make the struct and required feild pub
-        seasonal_fruit: String
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches")
-            }
-        }
-    }
-
-    //---------------------------------
-}
-
+mod front_of_house;
 use self::front_of_house::hosting;
 
 fn eat_at_restaurant() {
